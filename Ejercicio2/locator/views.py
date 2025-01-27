2a5311289f83f88afdd2500683022d5497abf136
GeraVillla <a22216369@alumnos.uady.mx>
2025-01-26 23:56:43 -0600

    feat(Ejercicio2): add redirect to original url feature

--- Ejercicio2/locator/views.py
+++ Ejercicio2/locator/views.py
@@ -5,6 +5,7 @@ from .models import Url
 from .serializer import UrlSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.authentication import get_authorization_header
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import authentication_classes, permission_classes
@@ -15,21 +16,6 @@ from rest_framework import status
 from .services import shorten_url
 from .services import is_unique_public_url
 from .services import get_unique_short_url
-"""
-@api_view(['POST'])
-def create_short_url(request):
-    try:
-        if request.data['original_url'] is "":
-            return Response({'error': 'Original URL is required'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if not request.data['is_public']:
-            return createUrlPrivate(request)
-        else:
-            return createUrlPublic(request)
-    except KeyError:
-        return Response({'error': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
-        
-"""
 
 @api_view(['POST'])
 def create_public_url(request):
@@ -42,6 +28,7 @@ def create_public_url(request):
     url = Url(original_url=original_url, short_url=short_url)
     url.save()
     return Response({'original_url' : original_url, 'short_url': short_url}, status=status.HTTP_201_CREATED)
+
 
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication])
@@ -61,6 +48,11 @@ def redirect_to_url(request, short_code):
     #short_code = request.build_absolute_uri()  
     short_code = request.get_host() + '/' + short_code
     url = get_object_or_404(Url, short_url=short_code)
+    auth_header = get_authorization_header(request).decode('utf-8')  # Obtener el encabezado de autorización
+    if url.is_public == False and auth_header and auth_header.startswith('Token'):
+        token = auth_header.split(' ')[1]
+        if token != url.user.auth_token.key:
+            return Response({'error': 'You do not have permission to access this url'}, status=status.HTTP_403_FORBIDDEN)
     url.clicks += 1
     url.save()
     return redirect(url.original_url)
