bdeacded921b4a8d74670b5715c906d95c4f362b
GeraVillla <A22216369@alumnos.uady.mx>
2025-01-28 21:26:18 -0600

    doc(Ejercicio2):  add Swagger documentation for authentication views

--- Ejercicio2/authentication/views.py
+++ Ejercicio2/authentication/views.py
@@ -9,6 +9,20 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+@swagger_auto_schema(
+    method='post',
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'email': openapi.Schema(type=openapi.TYPE_STRING),
+            'password': openapi.Schema(type=openapi.TYPE_STRING, format='password'),
+        },
+        required=['email', 'password']
+    ),
+    responses={200: 'Login successful', 400: 'Bad request'}
+)
 @api_view(['POST'])
 def login(request):
     if 'email' not in request.data or 'password' not in request.data:
@@ -19,7 +33,18 @@ def login(request):
     token, created = Token.objects.get_or_create(user=user)
     return Response({"token" : token.key}, status=status.HTTP_200_OK)
 
-
+@swagger_auto_schema(
+    method='post',
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'email': openapi.Schema(type=openapi.TYPE_STRING),
+            'password': openapi.Schema(type=openapi.TYPE_STRING, format='password'),
+        },
+        required=['email', 'password']
+    ),
+    responses={200: 'Login successful', 400: 'Bad request'}
+)
 @api_view(['POST'])
 def register(request):
     serializer = UserSerializer(data=request.data)
@@ -32,7 +57,18 @@ def register(request):
         return Response({'token' : token.key, 'email' : serializer.data,}, status=status.HTTP_201_CREATED)
     return Response({"Error" : serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-
+@swagger_auto_schema(
+    method='post',
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'email': openapi.Schema(type=openapi.TYPE_STRING),
+            'password': openapi.Schema(type=openapi.TYPE_STRING, format='password'),
+        },
+        required=['email', 'password']
+    ),
+    responses={200: 'Login successful', 400: 'Invalid input or credentials'}
+)
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
